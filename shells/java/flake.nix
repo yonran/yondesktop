d8c52aeee4f5a69621b79d2871d3b53969ef9bad
Yonathan Randolph <yonathan@gmail.com>
2022-08-23 11:48:07 -0700

    Add java shells

---
+++ shells/java/flake.nix
@@ -0,0 +1,34 @@
+# Usage with .envrc: use flake ./shells/java#jdk8
+{
+  description = "Shells with JDK, maven, sbt";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
+    flake-utils.url = "github:numtide/flake-utils/master";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      shellWithJdk = jdk: pkgs.mkShell {
+        buildInputs = [
+          jdk
+          (pkgs.sbt.override {
+            jre = jdk;
+          })
+          (pkgs.maven.override {
+            inherit jdk;
+          })
+
+          # keep this line if you use bash
+          pkgs.bashInteractive
+        ];
+      };
+      in {
+        devShells.jdk8 = shellWithJdk pkgs.openjdk8;
+        devShells.jdk9 = shellWithJdk pkgs.openjdk9;
+        devShells.jdk11 = shellWithJdk pkgs.openjdk11;
+        devShells.jdk13 = shellWithJdk pkgs.openjdk13;
+        devShells.jdk17 = shellWithJdk pkgs.openjdk17;
+      }
+    );
+}
