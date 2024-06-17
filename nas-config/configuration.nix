b72bb349f8bd4394c5451e429a7859e1e073262c
Yonathan Randolph <yonathan@gmail.com>
2024-06-17 07:06:15 -0700

    nas: Set timeZone

--- nas-config/configuration.nix
+++ nas-config/configuration.nix
@@ -108,7 +108,7 @@
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "America/Los_Angeles";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
