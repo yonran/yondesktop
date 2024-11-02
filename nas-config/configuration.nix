9fba9ad5d7e2d3103849c37410ce7daf4b87d160
Yonathan Randolph <yonathan@gmail.com>
2024-11-02 15:54:52 -0700

    Add samba smb

--- nas-config/configuration.nix
+++ nas-config/configuration.nix
@@ -19,6 +19,7 @@
       ./hardware-configuration.nix
       ./zfs-backup-module.nix
       ./home-monitoring.nix
+      ./owntracks-recorder-service.nix
     ];
   # enable zfs-backup-module
   services.zfsBackup = {
@@ -144,6 +145,54 @@
     workstation = true; # publish the machine as a workstatino, which includes the hostname
   };
 
+  # enable samba and samba-wsdd https://nixos.wiki/wiki/Samba
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    shares = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        #"use sendfile" = "yes";
+        #"max protocol" = "smb2";
+        # note: localhost is the ipv6 localhost ::1
+        "hosts allow" = "192.168.1. 127.0.0.1 localhost 192.168.29.";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+      "public" = {
+        "path" = "/firstpool/family/media";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "yonran";
+        "force group" = "users";
+      };
+      "private" = {
+        # smbpasswd -a username
+        "path" = "/firstpool/family/privateshare";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "yonran";
+        "force group" = "users";
+      };
+    };
+  };
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
+
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
@@ -261,6 +310,7 @@
     serviceConfig.ExecStart = "${pkgs.hdparm}/bin/hdparm -S 10 /dev/%I";
   };
 
+  # services.owntracks-recorder.enable = true;
 
 
   # Copy the NixOS configuration file and link it from the resulting system
