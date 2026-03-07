fb02aaf1748fbd0c2e268cb3d94b9a69171a2385
Yonathan Randolph <yonathan@gmail.com>
2026-03-06 20:13:22 -0800

    Install jq on server

--- nas-config/configuration.nix
+++ nas-config/configuration.nix
@@ -856,6 +856,7 @@ in
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     docker-compose
+    jq
     ntfs3g
     tmux
     dvdbackup
